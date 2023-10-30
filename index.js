cff6fc4ce0fd10aee91eebc31a36abf25ed760e4
Kenneth <luo.kenneth1999@gmail.com>
2023-10-29 23:48:42 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+var num1 = 2;
+var num2 = 31;
+var num3 = 24;
+var num4 = 5;
+var multiply = num1 * num2;
 
-// Write your code here
+var random = Math.floor(Math.random() * 10) + 1;
+
+var mod = num3 % num4;
+
+var max = Math.max(1, 2, 3, 4, 5, 20);
